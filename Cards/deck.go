2082285175b79f9607f2b101f5fa5dd14363d87d
Varnika Bagaria <varnika.bagaria@Varnikas-MacBook-Pro.local>
2022-10-19 14:48:24 +0530

    [Varnika] Add. create a new deck of cards

--- Cards/deck.go
+++ Cards/deck.go
@@ -5,6 +5,20 @@ import "fmt"
 // Create a new type deck which is slice of string
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := deck{"Spades", "Diamonds", "Clubs", "Hearts"}
+	cardValues := deck{"Ace", "Two", "Three"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" Of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
--- Cards/main.go
+++ Cards/main.go
@@ -2,13 +2,7 @@ package main
 
 func main() {
 	//var card string = "Ace of Spades"
-	cards := deck{newCard(), "Two"}
-	cards = append(cards, "Three")
-
+	cards := newDeck()
 	cards.print()
 
-}
-
-func newCard() string {
-	return "One of Diamonds"
 }
