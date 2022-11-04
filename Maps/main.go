3349646f820a02cf371505371f1130a4610232e6
Varnika Bagaria <varnika.bagaria@Varnikas-MacBook-Pro.local>
2022-11-04 13:14:00 +0530

    [Varnika] Add. add element in map

--- Maps/main.go
+++ Maps/main.go
@@ -4,10 +4,14 @@ import "fmt"
 
 func main() {
 	//map[key]value{k1:v1,}
+
+	//colors := make(map[string]string)
+	// var colors map[string]string
 	colors := map[string]string{
 		"red":   "0xFF0000",
 		"blue":  "0x00FF00",
 		"green": "0x0000FF",
 	}
+	colors["white"] = "0xFFFFFF"
 	fmt.Println(colors)
 }
