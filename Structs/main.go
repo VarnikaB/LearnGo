91897b7037c9926b7a08d7fdd9e6d953a2cceaaa
Varnika Bagaria <varnika.bagaria@Varnikas-MacBook-Pro.local>
2022-11-04 10:46:29 +0530

    [Varnika] Add. ways of assigning values to structure

--- Structs/main.go
+++ Structs/main.go
@@ -1,10 +1,21 @@
 package main
 
+import "fmt"
+
 type person struct {
-	firstName  string
-	secondName string
+	firstName string
+	lastName  string
 }
 
 func main() {
+	//var alex person -> create a structure alex with values as  "" ""
+	//default values: int, float: 0;;;bool: false;;;string: ""
+	alex := person{firstName: "Alex", lastName: "Anderson"}
+	fmt.Println(alex)
+	fmt.Printf("%+v\n", alex) // prints the field names with values
 
+	var alice person
+	alice.firstName = "Alice" // = not := as the field are already declared
+	alice.lastName = "Sanderson"
+	fmt.Printf("%+v\n", alice)
 }
