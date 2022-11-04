b5d1c1b70adc785c6aae1500de3f01b976b72f18
Varnika Bagaria <varnika.bagaria@Varnikas-MacBook-Pro.local>
2022-11-04 10:50:30 +0530

    [Varnika] Add. struct in a struct

--- Structs/main.go
+++ Structs/main.go
@@ -2,20 +2,29 @@ package main
 
 import "fmt"
 
+type contact struct {
+	email   string
+	zipcode int
+}
+
 type person struct {
-	firstName string
-	lastName  string
+	firstName   string
+	lastName    string
+	contactInfo contact
 }
 
 func main() {
 	//var alex person -> create a structure alex with values as  "" ""
 	//default values: int, float: 0;;;bool: false;;;string: ""
-	alex := person{firstName: "Alex", lastName: "Anderson"}
+	alex := person{
+		firstName: "Alex",
+		lastName:  "Anderson",
+		contactInfo: contact{
+			email:   "alex.anderson@gmail.com",
+			zipcode: 564091,
+		},
+	}
 	fmt.Println(alex)
 	fmt.Printf("%+v\n", alex) // prints the field names with values
 
-	var alice person
-	alice.firstName = "Alice" // = not := as the field are already declared
-	alice.lastName = "Sanderson"
-	fmt.Printf("%+v\n", alice)
 }
